7adefec95524ad27fb78fe6801ad865e9d1d7095
nguyenquanbn1412 <148620904+nguyenquanbn1412@users.noreply.github.com>
2024-04-20 16:42:40 +0700

    Create Lab3_b1.js

---
+++ Lab3_b1.js
@@ -0,0 +1,6 @@
+let p = document.createElement("p");
+p.id = "text";
+p.innerHTML = "Tôi có thể làm <em> bất cứ điều gì </em> tôi muốn với JavaScript.";
+p.style.color = "#777";
+p.style.fontSize = "2rem";
+document.body.appendChild(p);
